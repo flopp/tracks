aeb5c192758178411cefa7af9c84591cf72ed3db
flopp <fpigorsch@gmail.com>
2019-01-21 18:49:55 +0100

    add garmin syncing

--- main.py
+++ main.py
@@ -7,9 +7,27 @@
 
 
 import argparse
+import datetime
+import os
 
 from src.htmlwriter import HtmlWriter
 from src.tracks import Tracks
+from vendor.garminexport.garminexport import (backup, garminclient, retryer)
+
+
+def sync(data_dir, username, password):
+    if not os.path.isdir(data_dir):
+        os.makedirs(data_dir)
+    retry = retryer.Retryer(
+        delay_strategy=retryer.ExponentialBackoffDelayStrategy(
+            initial_delay=datetime.timedelta(seconds=1)),
+        stop_strategy=retryer.MaxRetriesStopStrategy(2))
+    formats = ['fit']
+    with garminclient.GarminClient(username, password) as client:
+        activities = set(retry.call(client.list_activities))
+        missing_activities = backup.need_backup(activities, data_dir, formats)
+        for activity in missing_activities:
+            backup.download(client, activity, retry, data_dir, formats)
 
 
 def main():
@@ -23,6 +41,11 @@ def main():
     args_parser.add_argument('--clear-cache', dest='clear_cache', action='store_true')
     args = args_parser.parse_args()
 
+    print('syncing')
+    GARMIN_ACCOUNT = os.environ['GARMIN_ACCOUNT']
+    GARMIN_PASSWORD = os.environ['GARMIN_PASSWORD']
+    sync(os.path.join(args.data_dir, 'garmin-connect'), GARMIN_ACCOUNT, GARMIN_PASSWORD)
+    
     print('loading')
     t = Tracks()
     t.set_poi_file('poi.txt')
